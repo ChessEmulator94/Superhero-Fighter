fb81266f8ff319a5f5bb101a06218a332cd4ef4d
Gadi <guppiefriedman@gmail.com>
2024-03-02 09:04:17 +0200

    Formatting

--- Front End/UserInterfaceAPIs.js
+++ Front End/UserInterfaceAPIs.js
@@ -1,33 +1,37 @@
 // Set equal to the URL server is running on
-const SERVER_URL = `http://localhost:5500`
+const SERVER_URL = `http://localhost:5500`;
 
 // Query the node.js server @queryURL with a GET
 // Functionality @ app.get("/viewheroes" ...) in index.js
 const viewAllHeroes = () => {
   let queryURL = `${SERVER_URL}/viewheroes`;
-  fetch(queryURL, {})
-     .then(response => response.text())
-     .then(text => {
-      console.log(JSON.stringify(text)); 
-      return text
-    })
+  return fetch(queryURL, {})
+    .then((response) => response.text())
+    .then((text) => {
+      //console.log(JSON.stringify(text));
+      return text;
+    });
 };
 
 // Query the node.js server @queryURL with a POST
 // Functionality @ app.use("/addhero" ...) in index.js
-async function addHero(){  
-
-  let queryURL = `${SERVER_URL}/addhero`;
-  let urlBody =  JSON.stringify({ "name": "Sherlock Gnomes", "age":"222" });
+async function addHero() {
+  let heroID = 3;
+  let queryURL = `${SERVER_URL}/addhero/${heroID}`;
+  let urlBody = JSON.stringify({ name: "Sherlock Gnomes", age: "222" });
 
   fetch(queryURL, {
-     method: 'POST',
-     headers: {
-      'Content-Type': 'application/json'
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-     body: urlBody
-   }).then(response => response.text).then(text => {return text});
-};
+    body: urlBody,
+  })
+    .then((response) => response.text)
+    .then((text) => {
+      return text;
+    });
+}
 
 // Query the node.js server @queryURL with a DELETE
 const deleteHero = () => {
