1dd601d2baa5756731dc56bd2cec70a3a6258936
Gadi <guppiefriedman@gmail.com>
2024-03-02 08:58:55 +0200

    Removed redundant code

--- REST Logic/HeroApiHandler.js
+++ REST Logic/HeroApiHandler.js
@@ -1,29 +1,25 @@
 /* View possible Superhero IDs @ https://superheroapi.com/ids.html */
 
-
 // Set to your access token from superheroapi.com
 const ACCESS_TOKEN = 7150306591723520;
 // Base URL for all API requests
 const BASE_URL = `https://superheroapi.com/api/` + ACCESS_TOKEN;
 
-// Placeholder for DB (add later)
-let allHeros = [];
-
-// Add data to the superhero database
-function addHeroToDatabase(heroData) {
-  allHeros.push(JSON.stringify(heroData));
-  console.log(`Hero with ID ${heroData.id} and name ${heroData.name} added.`);
-}
-
 // Gets Superhero data based on provided ID
 function getHeroData(targetHeroID) {
   let fetchURL = `${BASE_URL}/${targetHeroID}`;
-  
+
   return fetch(fetchURL)
     .then((response) => response.json())
     .then((heroData) => {
-      //addHeroToDatabase(heroData);
-      return heroData;
+      let summaryJSON = {
+        id: heroData.id,
+        name: heroData.name,
+        powerstats: heroData.powerstats,
+        image: heroData.image,
+      };
+
+      return summaryJSON;
     })
     .catch((error) => {
       console.error("Error fetching hero data:", error);
@@ -31,7 +27,6 @@ function getHeroData(targetHeroID) {
     });
 }
 
- 
 async function main() {
   let tempData = await getHeroData(5);
   console.log(tempData);
